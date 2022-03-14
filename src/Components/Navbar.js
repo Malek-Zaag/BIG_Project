8d642461c36caa1ef0f545046c81583da8a44cb1
Malek-Zaag <zaag.malek1@gmail.com>
2022-03-14 23:02:36 +0100

    styling navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,24 @@
+import { Button, Container } from '@mui/material'
 import React from 'react'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <section style={{ backgroundColor: "#0000CD", display: "flex", alignItems: "center", justifyContent: "space-between", }}>
+        <div>
+          <Button variant="contained" color="primary" size="large">logo</Button>
+        </div>
+        <div>
+          <Button variant="contained" style={{ margin: "16px" }} color="primary" size="large" href="/">Home</Button>
+          <Button variant="contained" style={{ margin: "16px" }} color="warning" size="large" href="shop" endIcon={<KeyboardArrowDownIcon />}>Shop</Button>
+          <Button variant="contained" style={{ margin: "16px" }} color="success" size="large" href="cart">Cart</Button>
+          <Button href="login"><AccountCircleIcon fontSize="large"></AccountCircleIcon></Button>
+        </div>
+
+      </section>
+    </div>
   )
 }
 
