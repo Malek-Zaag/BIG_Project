c1607a6d142117f5f6a71b9a0629dd0f7c4b12ba
Malek-Zaag <zaag.malek1@gmail.com>
2022-03-14 21:22:59 +0100

    config

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Navbar from './Components/Navbar';
 function App() {
   return (
     <div className="App">
-
-      <h1 className="text-5xl font-mono text-orange underline">Hello World</h1>
-      
+      <Navbar></Navbar>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,9 @@
 import React from 'react'
-import '../App.css'
 
 const Navbar = () => {
-    return (
-        <nav className="flex bg-orange sm:justify-center h-9 space-x-4">
-            <div className=" flex">
-                <div>
-                    <a></a>
-                    <img></img>
-                </div>
-                {[
-                    ['Home', '/home'],
-                    ['Shop', '/shop'],
-                    ['Cart', '/cart'],
-                    ['Account', '/account'],
-                ].map(([title, url]) => (
-                    <a href={url} className="font-size-5 px-5 my-2 text-indigo">{title}</a>
-                ))}
-            </div>
-        </nav>
-    )
+  return (
+    <div>Navbar</div>
+  )
 }
 
 export default Navbar
