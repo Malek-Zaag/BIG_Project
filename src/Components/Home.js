ca3080f02a048d7f28ca8c02852648ee2f6b6dd8
Malek-Zaag <zaag.malek1@gmail.com>
2022-03-15 09:36:21 +0100

    navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <div>
         <Navbar></Navbar>
+        <section style={{backgroundColor: 'green'}}>
+            <div>hii</div>
+        </section>
     </div>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,21 +2,22 @@ import { Button, Container } from '@mui/material'
 import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import logo from '../images/logo.png'
+
 
 const Navbar = () => {
   return (
     <div>
       <section style={{ backgroundColor: "#0000CD", display: "flex", alignItems: "center", justifyContent: "space-between", }}>
         <div>
-          <Button variant="contained" color="primary" size="large">logo</Button>
+          <Button  href="/"><img src={logo} style={{ width: "15vw", height: "35vh" }} alt="logo"></img></Button>
         </div>
         <div>
-          <Button variant="contained" style={{ margin: "16px" }} color="primary" size="large" href="/">Home</Button>
-          <Button variant="contained" style={{ margin: "16px" }} color="warning" size="large" href="shop" endIcon={<KeyboardArrowDownIcon />}>Shop</Button>
-          <Button variant="contained" style={{ margin: "16px" }} color="success" size="large" href="cart">Cart</Button>
-          <Button href="login"><AccountCircleIcon fontSize="large"></AccountCircleIcon></Button>
+          <Button  variant="outlined" style={{ margin: "16px" }} color="secondary" size="large" href="/">Home</Button>
+          <Button  variant="contained" style={{ margin: "16px" }} color="warning" size="large" href="shop" endIcon={<KeyboardArrowDownIcon />}>Shop</Button>
+          <Button  variant="contained" style={{ margin: "16px" }} color="success" size="large" href="cart">Cart</Button>
+          <Button  variant="contained" style={{ margin: "16px" }} color="warning" size="large" href="login">Login</Button>
         </div>
-
       </section>
     </div>
   )
