caf2ae364ada825036c424c075ab5ce2347497bf
Malek-Zaag <zaag.malek1@gmail.com>
2022-03-16 10:12:30 +0100

    adding some routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
+import Login from './Pages/Login';
+import Shop from './Pages/Shop';
 import Aos from 'aos'
-import { Route,Router, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 
 function App() {
   Aos.init()
   return (
     <Switch>
+
       <Route exact path="/"><Home/></Route>
       <Route exact path="/cart"><Cart/></Route>
+      <Route exact path="/shop"><Shop/></Route>
+      <Route exact path="/login"><Login/></Route>
     </Switch>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,23 @@
-import { Container, Typography, Grid, CardContent, Card, Button,Box } from '@mui/material'
+import { Container, Typography, Grid, CardContent, Card, Button, Box } from '@mui/material'
 import React, { } from 'react'
 import Navbar from '../Components/Navbar'
 import logo from '../images/logo.png'
+import login from '../images/login.png'
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
   color: {
     backgroundColor: "#0000CD",
   },
-  
+
 })
 
 const Home = () => {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <div>
       <Navbar></Navbar>
-      <Grid container spacing={2} justify="center" alignItems="center" className={classes.color}  >
+      <Grid container spacing={2} sx={{ paddingTop: '175px' }} justify="center" alignItems="center" className={classes.color}  >
         <Grid className={classes.color} item xs={12} md={6}>
           <Card sx={{ border: "none", boxShadow: "none" }}>
             <CardContent style={{ textAlign: 'center', backgroundColor: "#0000CD", }}>
@@ -34,20 +35,44 @@ const Home = () => {
                 cupidatat non proident, sunt in
                 culpa qui officia deserunt mollit anim id est laborum
               </Typography>
-              <Box sx={{display:"flex", alignItems: "center",justifyContent: "center"}}>
+              <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                 <Button href="/shop" sx={{ margin: "30px 0 0 0", }} variant="contained" color="warning">Shop</Button>
               </Box>
             </CardContent>
           </Card>
         </Grid>
       </Grid>
-      <Box sx={{height: "200px",bgcolor: 'warning.main'}}></Box>
+      <Box sx={{ height: "300px", bgcolor: 'warning.main' }}></Box>
+      <Grid container spacing={2} justify="center" alignItems="center" className={classes.color}  >
+        <Grid data-aos="fade-right" className={classes.color} item xs={12} md={6}>
+          <Card sx={{ border: "none", boxShadow: "none" }}>
+            <CardContent style={{ textAlign: 'center', backgroundColor: "#0000CD", }}>
+              <img src={login} alt="login" style={{ width: "300px", height: "300px", }}>
+              </img>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid sx={{ border: 0 }} item xs={12} md={6}>
+          <Card data-aos="fade-up" sx={{ border: "none", boxShadow: "none" }} className={classes.color}>
+            <CardContent className={classes.color}>
+              <Typography variant="h6">Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam
+                cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum
+              </Typography>
+              <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <Button href="/login" sx={{ margin: "30px 0 0 0", }} variant="contained" color="warning">Login</Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      <Box sx={{ height: "300px", bgcolor: 'warning.main' }}></Box>
       <Grid container spacing={2} justify="center" alignItems="center" className={classes.color}  >
         <Grid className={classes.color} item xs={12} md={6}>
           <Card sx={{ border: "none", boxShadow: "none" }}>
-            <CardContent style={{ textAlign: 'center', backgroundColor: "#0000CD", }}>
-              <img src={logo} alt="logo" style={{ width: "300px", height: "300px", }}>
-              </img>
+            <CardContent id="map" style={{ textAlign: 'center', backgroundColor: "#0000CD", }}>
             </CardContent>
           </Card>
         </Grid>
@@ -60,15 +85,13 @@ const Home = () => {
                 cupidatat non proident, sunt in
                 culpa qui officia deserunt mollit anim id est laborum
               </Typography>
-              <Box sx={{display:"flex", alignItems: "center",justifyContent: "center"}}>
-                <Button href="/shop" sx={{ margin: "30px 0 0 0", }} variant="contained" color="warning">Shop</Button>
+              <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                <Button href="/login" sx={{ margin: "30px 0 0 0", }} variant="contained" color="warning">Login</Button>
               </Box>
             </CardContent>
           </Card>
         </Grid>
       </Grid>
-      
-
     </div>
   )
 }
