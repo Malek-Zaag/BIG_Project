95ca39566a4406c8e065aefdcf6fd639ce83ee67
Malek-Zaag <zaag.malek1@gmail.com>
2022-03-18 18:46:03 +0100

    finishing homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,23 +3,16 @@ import React, { } from 'react'
 import Navbar from '../Components/Navbar'
 import logo from '../images/logo.png'
 import login from '../images/login.png'
-import { makeStyles } from '@mui/styles';
+import location from '../images/location.png'
+import classes from '../Components/Style.module.css'
 
-
-
-const useStyles = makeStyles({
-  color: {
-    backgroundColor: "#0000CD",
-  },
-
-})
 
 const Home = () => {
-  const classes = useStyles();
+
   return (
     <div>
       <Navbar></Navbar>
-      <Grid container spacing={2} sx={{ paddingTop: '175px'}} justify="center" alignItems="center" className={classes.color}  >
+      <Grid container spacing={2} sx={{ paddingTop: '175px' }} justify="center" alignItems="center" className={classes.color}  >
         <Grid className={classes.color} item xs={12} md={6}>
           <Card sx={{ border: "none", boxShadow: "none" }}>
             <CardContent style={{ textAlign: 'center', backgroundColor: "#0000CD", }}>
@@ -31,11 +24,13 @@ const Home = () => {
         <Grid sx={{ border: 0 }} item xs={12} md={6}>
           <Card sx={{ border: "none", boxShadow: "none" }} className={classes.color}>
             <CardContent className={classes.color}>
-              <Typography variant="h6">Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam
-                cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum
+              <Typography gutterBottom fontFamily="Fredoka" align="left" variant="h2">About Us</Typography>
+              <Typography gutterBottom align="left" fontWeight="bold" variant="h5" fontFamily="Fredoka">
+                Our site is one of the most famous 
+                sites in this field . We sell all 
+                types of musical instruments alongside
+                with their accessories . Feel free to 
+                contact us at our email.
               </Typography>
               <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                 <Button href="/shop" sx={{ margin: "30px 0 0 0", }} variant="contained" color="warning">Shop</Button>
@@ -45,7 +40,7 @@ const Home = () => {
         </Grid>
       </Grid>
       <Box sx={{ height: "300px", bgcolor: 'warning.main' }}></Box>
-      <Grid container spacing={2} sx={{borderRadius:"16px"}} justify="center" alignItems="center"  className={classes.color}  >
+      <Grid container spacing={2} justify="center" alignItems="center" className={classes.color}  >
         <Grid data-aos="fade-right" className={classes.color} item xs={12} md={6}>
           <Card sx={{ border: "none", boxShadow: "none" }}>
             <CardContent style={{ textAlign: 'center', backgroundColor: "#0000CD", }}>
@@ -57,11 +52,12 @@ const Home = () => {
         <Grid sx={{ border: 0 }} item xs={12} md={6}>
           <Card data-aos="fade-up" sx={{ border: "none", boxShadow: "none" }} className={classes.color}>
             <CardContent className={classes.color}>
-              <Typography variant="h6">Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam
-                cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum
+              <Typography gutterBottom fontFamily="Fredoka" align="left" variant="h2">Join US</Typography>
+              <Typography gutterBottom align="left" fontWeight="bold" variant="h5" fontFamily="Fredoka">
+                In order to buy something , you 
+                need to login into your account 
+                or create a new one if you didn't 
+                already .
               </Typography>
               <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                 <Button href="/login" sx={{ margin: "30px 0 0 0", }} variant="contained" color="warning">Login</Button>
@@ -71,25 +67,35 @@ const Home = () => {
         </Grid>
       </Grid>
       <Box sx={{ height: "300px", bgcolor: 'warning.main' }}></Box>
-      <Grid data-aos="fade-up" container spacing={2} justify="center" alignItems="center" className={classes.color}  >
-        <Grid  className={classes.color} item xs={12} md={6}>
+      <Grid container spacing={2} justify="center" alignItems="center" className={classes.color}  >
+        <Grid className={classes.color} item xs={12} md={6}>
           <Card sx={{ border: "none", boxShadow: "none" }}>
-            <CardContent  id="map" style={{ textAlign: 'center', backgroundColor: "#0000CD", }}>
-              <img style={{width: "250px", height: "250px"}} alt="map" src="https://maps.googleapis.com/maps/api/staticmap?size=200x200&scale=1&format=png&maptype=roadmap&key=AIzaSyCFijUH2ZKhTlTPkHnpGFMe9aGA6xUL-cM&markers=size:normal%7Ccolor:blue%7Clabel:P%7C36.85413801585305,10.263616836230094"></img>
+            <CardContent id="map" style={{ textAlign: 'center', backgroundColor: "#0000CD", }}>
+              <img style={{ width: "250px", height: "250px" }} alt="map" src={location}></img>
             </CardContent>
           </Card>
         </Grid>
         <Grid sx={{ border: 0 }} item xs={12} md={6}>
-          <Card  sx={{ border: "none", boxShadow: "none" }} className={classes.color}>
+          <Card data-aos="flip-left" sx={{ border: "none", boxShadow: "none" }} className={classes.color}>
             <CardContent className={classes.color}>
-              <Box component='span' sx={{  p: 2, border: '1px dashed grey' ,display: "flex", alignItems: "center", justifyContent: "center" }}>    
+            <Typography gutterBottom fontFamily="Fredoka" align="left" variant="h2" >INFO BOX</Typography>
+              <Box component='span' sx={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
+                <Typography fontFamily="Fredoka" fontWeight="bold" variant="h5">
+                  Address : Rue Sidi Ahmed Aouina, Tunis 2045, Tunisia
+                  <br />
+                  Twitter: <a href='https://twitter.com/ZaagMalek'>https://twitter.com/ZaagMalek</a>
+                  <br />
+                  Instagram: <a href='https://www.instagram.com/zaagmelek/'>https://www.instagram.com/zaagmelek/</a>
+                  <br />
+                  Linkedin: <a href='https://www.linkedin.com/in/melekzaag/'>https://www.linkedin.com/in/melekzaag/</a>
+                </Typography>
               </Box>
             </CardContent>
           </Card>
         </Grid>
       </Grid>
-      <footer style={{textAlign:"center"}}>
-        Copyrigths@Malek2022
+      <footer style={{ textAlign: "center" }}>
+        <Typography gutterBottom fontFamily="Fredoka" variant="h5">Copyrigths@Malek2022</Typography>
       </footer>
     </div>
   )
