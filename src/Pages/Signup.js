de66635ed533a0696429cbb92274e802fc025f47
Malek-Zaag <zaag.malek1@gmail.com>
2022-03-18 14:38:05 +0100

    editing signup page

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,20 +1,37 @@
-import { TextField } from '@mui/material'
+import { Box, Button, Grid, TextField } from '@mui/material'
 import React from 'react'
 import Navbar from '../Components/Navbar'
 
 const Signup = () => {
-  return (
-    <div>
-        <Navbar></Navbar>
-        <form action='/signup' method='POST'>
-            <TextField>
-                <input type="email"></input>
-                <input type="text">text</input>
-            </TextField>
-
-        </form>
-    </div>
-  )
+    const handleClick = (e) => {
+        e.preventDefault()
+        console.log('hey')
+    }
+    return (
+        <div>
+            <Navbar></Navbar>
+            <form action='/signup' method='POST'>
+                <Box sx={{ display: 'flex', flexDirection: "column", alignItems: "center", marginTop: 11 }}>
+                    <Box sx={{m: 4}}>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        </Grid>
+                    </Grid>
+                    </Box>
+                </Box>
+            </form>
+        </div>
+    )
 }
 
 export default Signup
