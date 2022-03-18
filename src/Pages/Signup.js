b40128d9b3f414a083fdb01955601eb416c70d97
Malek-Zaag <zaag.malek1@gmail.com>
2022-03-18 17:16:46 +0100

    finishing signup page

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, TextField } from '@mui/material'
+import { Button, Grid, TextField, Container,FormControlLabel,FormLabel,RadioGroup,FormControl,Radio } from '@mui/material'
 import React from 'react'
 import Navbar from '../Components/Navbar'
 
@@ -10,27 +10,37 @@ const Signup = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <form action='/signup' method='POST'>
-                <Box sx={{ display: 'flex', flexDirection: "column", alignItems: "center", marginTop: 11 }}>
-                    <Box sx={{m: 4}}>
-                    <Grid container spacing={1}>
+            <Container style={{ position: "relative", backgroundColor: "#D0D0D0", marginTop: "100px", padding: "100px", borderRadius: "16px" }}>
+                <form action='/signup' method='POST'>
+                    <Grid container spacing={5}>
                         <Grid item xs={12} sm={6}>
-                            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                            <TextField id="outlined-basic" label="First Name" variant="outlined" fullWidth />
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                            <TextField id="outlined-basic" label="Last Name" variant="outlined" fullWidth />
                         </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        <Grid item xs={12} >
+                            <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth />
                         </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                        <Grid item xs={12} >
+                            <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl>
+                                <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+                                <RadioGroup aria-labelledby="demo-radio-buttons-group-label"defaultValue="female" name="radio-buttons-group">
+                                    <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                    <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                </RadioGroup>
+                            </FormControl>
+                        </Grid>
+                        <Grid sx={{display: 'flex',flexDirection: "row", justifyContent: "flex-end"}} item xs={12}>
+                            <Button type='submit' onClick={handleClick} variant="contained" color="warning">submit</Button>
                         </Grid>
                     </Grid>
-                    </Box>
-                </Box>
-            </form>
-        </div>
+                </form>
+            </Container>
+        </div >
     )
 }
 
