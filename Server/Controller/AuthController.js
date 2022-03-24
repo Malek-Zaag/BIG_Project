d457fea2c2c368b7893e468be366cb49566a259e
Malek-Zaag <zaag.malek1@gmail.com>
2022-03-24 17:10:52 +0100

    adding jwt

--- Server/Controller/AuthController.js
+++ Server/Controller/AuthController.js
@@ -1,12 +1,23 @@
 const User = require("../Model/User")
+const jwt=require("jsonwebtoken")
 
-module.exports.singup=(req,res)=>{
+const maxAge=600*24*60*60
+const createToken= (id) =>{
+    return jwt.sign({id}, "secretkey" ,{
+        expiresIn: maxAge
+    })
+}
+
+module.exports.singup = (req, res) => {
 
     console.log(req.body)
-    const user=new User(req.body)
+    const user = new User(req.body)
     user.save()
-    .then((res)=> console.log("done posting to db"))
-    .catch((err)=>{console.log(err)})
-    
+        .then((res) => console.log("done posting to db"))
+        .catch((err) => { console.log(err) })
+    const token=createToken(user._id)
+    console.log(token)
     console.log('done')
+    //res.cookie('jwt', token, {httpOnly: true,maxAge: maxAge*1000})
+    //res.status(201).json({ user: user._id})
 }
--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,28 @@
-const express=require("express");
-const route=require("./Routes/AuthRoute")
-const PORT=process.env.PORT || 4000;
+const express = require("express");
+const route = require("./Routes/AuthRoute")
+const PORT = process.env.PORT || 4000;
 var cors = require("cors");
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
-const app=express()
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
+const app = express()
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
 
 //middleware
 app.use(express.json())
 app.use(cors(corsOptions));
+app.use(cookieParser)
+
 
 //db
-const dbURI="mongodb+srv://admin:admin@cluster0.gkmmv.mongodb.net/Sama3ni?retryWrites=true&w=majority"
-mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) =>{app.listen(PORT);console.log(`Server listening on ${PORT}`);})
-.catch((err)=>{ console.log(err)})
+const dbURI = "mongodb+srv://admin:admin@cluster0.gkmmv.mongodb.net/Sama3ni?retryWrites=true&w=majority"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => { app.listen(PORT); console.log(`Server listening on ${PORT}`); })
+    .catch((err) => { console.log(err) })
 
 
 
