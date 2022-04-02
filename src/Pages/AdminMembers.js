e652ae43d1aa93a36d0f6847b609ac8cd879eb3f
Malek-Zaag <zaag.malek1@gmail.com>
2022-04-02 17:07:30 +0100

    displaying users

--- src/Pages/AdminMembers.js
+++ src/Pages/AdminMembers.js
@@ -1,22 +1,35 @@
-import { Button, Grid } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import DashDrawer from '../Components/DashDrawer'
+import AdminDashboard from './AdminDashboard'
 
 const AdminMembers = () => {
     const [members, setMembers] = useState([])
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         fetch("http://localhost:4000/members")
             .then(res => res.json())
-            .then(result => setMembers(result))
+            .then(result => {
+                setMembers(result);
+                setLoading(false)
+            })
             .catch(err => console.log(err))
 
     }, [])
-    return (
-        <div>
-            <Button style={{ margin: "10px 35px 10px 35px" }} color="warning" size="medium" variant="contained" href="/">Home</Button>
-            <DashDrawer></DashDrawer>
-            <div>hii</div>
-        </div>
-    )
+    if (loading) return <div>Loading ....</div>
+    else
+        return (
+            <div>
+                <AdminDashboard></AdminDashboard>
+                <div>
+                    {members.map(member => (
+                        <div id={member._id}>
+                            <span>First Name: {member.firstname}</span>
+                            <span>Last Name: {member.lastname}</span>
+                            <span>Last Name: {member.email}</span>
+                            <span>Last Name: {member.gender}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
 }
 export default AdminMembers 
--- src/Pages/AdminProducts.js
+++ src/Pages/AdminProducts.js
@@ -1,6 +1,5 @@
 import { Button, Grid } from '@mui/material'
 import React from 'react'
-import DashDrawer from '../Components/DashDrawer'
 
 const AdminProducts = () => {
   return (
@@ -10,7 +9,7 @@ const AdminProducts = () => {
           <Button style={{ margin: "10px 35px 10px 35px" }} color="warning" size="medium" variant="contained" href="/">Home</Button>
         </Grid>
         <Grid item xs={12}>
-          <DashDrawer></DashDrawer>
+          
         </Grid>
       </Grid>
     </div>
