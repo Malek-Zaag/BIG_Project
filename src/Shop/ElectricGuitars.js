230de51f9f16e5389e4c5968167ccffd64bc4ed4
Malek-Zaag <zaag.malek1@gmail.com>
2022-04-03 15:38:36 +0100

    adding products to pages

--- src/Shop/ElectricGuitars.js
+++ src/Shop/ElectricGuitars.js
@@ -1,50 +1,58 @@
 import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminAvatar from '../Components/AdminAvatar'
 import Navbar from '../Components/Navbar'
-import test from "../images/test.jpg"
 
 const ElectricGuitars = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:4000/products")
+            .then(res => res.json())
+            .then(result => setProducts(result))
+            .catch(err => console.log(err))
+    }, [products])
     return (
         <div>
             <Navbar></Navbar>
-            <Grid sx={{ paddingTop: "20px" }} container spacing={3}>
-                <Grid item xs={12} md={3}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia
-                                alt='test'
-                                component="img"
-                                image={test}>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div"
-                                    fontWeight="bold" align="center" fontFamily="Fredoka">
-                                    Simple Guitar
-                                </Typography>
-                                <Typography gutterBottom variant="h5" component="div"
-                                    fontWeight="bold" fontFamily="Fredoka">
-                                    This guitar is simple
-                                </Typography>
-                                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                                    <Typography gutterBottom variant="h5" component="span"
-                                        fontWeight="bold" fontFamily="Fredoka">
-                                        Price:20$
-                                    </Typography>
-                                    <Typography gutterBottom variant="h5" component="span"
-                                        fontWeight="bold" sx={{ color: "green" }} fontFamily="Fredoka">
-                                        Stock:+20
-                                    </Typography>
-                                </div>
-                            </CardContent>
-                            <CardActions sx={{ display: "flex", justifyContent: "center" }}>
-                                <Button variant="outlined" align>Add to Cart</Button>
-                            </CardActions>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-            </Grid>
             <AdminAvatar></AdminAvatar>
+            <Grid sx={{ paddingTop: "20px" }} container spacing={3}>
+                {products.filter(product => product.category==="Drum").map(product => (
+                    <Grid item xs={12} md={3}>
+                        <Card id={product._id}>
+                            <CardActionArea>
+                                <CardMedia
+                                    alt='test'
+                                    component="img"
+                                    image={product.image}>
+                                </CardMedia>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div"
+                                        fontWeight="bold" align="center" fontFamily="Fredoka">
+                                        {product.name}
+                                    </Typography>
+                                    <Typography gutterBottom variant="h5" component="div"
+                                        fontWeight="bold" fontFamily="Fredoka">
+                                        {product.description}
+                                    </Typography>
+                                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                        <Typography gutterBottom variant="h5" component="span"
+                                            fontWeight="bold" fontFamily="Fredoka">
+                                            Price:{product.price}$
+                                        </Typography>
+                                        <Typography gutterBottom variant="h5" component="span"
+                                            fontWeight="bold" sx={{ color: "green" }} fontFamily="Fredoka">
+                                            Stock:{product.stock}
+                                        </Typography>
+                                    </div>
+                                </CardContent>
+                                <CardActions sx={{ display: "flex", justifyContent: "center" }}>
+                                    <Button variant='outlined' align color="success">Add TO Cart</Button>
+                                </CardActions>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     )
 }
