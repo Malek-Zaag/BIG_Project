891da1e6c59af70c9e0bcd7e6168a591842be9f8
Malek-Zaag <zaag.malek1@gmail.com>
2022-04-12 23:08:52 +0100

    chore: finished product page

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import axios from "axios"
-import { Grid, Card, Button, Typography, CardMedia, CardActionArea, CardContent, CardActions, Pagination } from "@mui/material"
+import { Grid, Button, Typography } from "@mui/material"
 import { useHistory } from 'react-router-dom'
 
 const ProductPage = ({ id }) => {
@@ -16,17 +16,27 @@ const ProductPage = ({ id }) => {
     }, [])
     return (
         <div>
-            <Navbar></Navbar>
-            <div>voila tu as reussi {id}</div>
-            <Grid conatiner spacing={1}>
-                <Grid item xs={8}>
-                    <img src={product.image} alt="img"></img>
+            <Typography fontWeight="bold" fontFamily="Fredoka">
+                <Navbar></Navbar>
+                <Grid container spacing={1}>
+                    <Grid item xs={12} sm={6}>
+                        <img style={{width: "100%"}} src={product.image} alt="img"></img>
+                    </Grid>
+                    <Grid style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center" }} item xs={12} sm={6}>
+                        <h1>test</h1>
+                        <div>{product.description}</div>
+                        <div style={{ color: "green" }}>{product.stock}</div>
+                        <div>{product.price}$</div>
+                        <div>
+                            <Button variant="contained">
+                                Add to Card
+                            </Button>
+                        </div>
+                    </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                    <Button>test</Button>
-                </Grid>
-            </Grid>
-        </div>
+            </Typography>
+
+        </div >
     )
 }
 
