fc27c01243efabbdb41eeaaddfb15382967ccccd
Malek-Zaag <zaag.malek1@gmail.com>
2022-04-16 00:55:27 +0100

    finished designing cart page

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
-import { Grid } from '@mui/material'
-
+import { Grid, Button } from '@mui/material'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import mastercard from '../images/icons8-mastercard-credit-card-48.png'
+import amex from '../images/icons8-american-express-48.png'
+import paypal from '../images/icons8-paypal-an-online-payments-system-operating-worldwide-48.png'
+import visa from '../images/icons8-visa-48.png'
 
 const Cart = () => {
   const [count, setCount] = useState(0)
@@ -12,17 +16,31 @@ const Cart = () => {
       <div style={{ backgroundColor: "#DCDCDC" }}>
         <Navbar></Navbar>
         <div>
-          <Grid style={{ marginTop: "2vh", display: "flex", justifyContent: "space-between" }} container spacing={2}>
+          <Grid style={{ marginTop: "1vw", paddingLeft: "1em", display: "flex", justifyContent: "space-between" }} container spacing={2}>
             <Grid item xs={12} sm={6} style={{ backgroundColor: "white", fontFamily: "Fredoka", fontWeight: "bold", fontSize: "2.5vw" }} >
-              <div>MY ITEMS</div>
-              <hr style={{ marginRight: "5vw" }} />
-
+              <div style={{ fontFamily: "Fredoka", fontWeight: "bold", fontSize: "1.5vw" }}>MY ITEMS</div>
+              <div style={{ marginRight: "5vw", fontFamily: "Fredoka", fontWeight: "bold", fontSize: "1.5vw" }}>
+                <hr />
+                <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                  <div style={{ display: "flex", justifyContent: "space-between" }}>SUB-TOTAL <span>{count} $</span> </div>        </div>
+              </div>
             </Grid>
             <Grid style={{ backgroundColor: "white" }} item xs={12} sm={6} >
-              <div style={{ fontFamily: "Fredoka", fontWeight: "bold", fontSize: "2.5vw" }}>TOTAL</div>
-              <div style={{ marginRight: "5vw", fontFamily: "Fredoka", fontWeight: "bold", }}>
+              <div style={{ fontFamily: "Fredoka", fontWeight: "bold", fontSize: "1.5vw" }}>TOTAL</div>
+              <div style={{ marginRight: "5vw", fontFamily: "Fredoka", fontWeight: "bold", fontSize: "1.5vw" }}>
                 <hr />
-                <div style={{ display: "flex", justifyContent: "space-between" }}>Sub-total  <span>{count}</span></div>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>Sub-total  <span>{count} $</span></div>
+                <br />
+                <div style={{ display: "flex", justifyContent: "space-between" }}>Delivery (free)<InfoOutlinedIcon></InfoOutlinedIcon></div>
+                <br />
+                <hr />
+                <br />
+                <Button size="large" fullWidth variant="contained" color="success">CHECKOUT</Button>
+                <br />
+                <br />
+                <div>WE ACCEPT:</div>
+                <br />
+                <div><img src={mastercard} alt='mastercard' /><img src={visa} alt='visa' /><img src={amex} alt='amex' /><img src={paypal} alt='paypal' /></div>
               </div>
             </Grid>
           </Grid>
